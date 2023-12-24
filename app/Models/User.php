381cb41f5e5ea1d639daa0fa5eeb78724265de35
juanyaolin <maple96541@gmail.com>
2023-12-25 00:25:44 +0800

    feat(User): modify migration and add uuid

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Traits\HasUuid;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -10,7 +11,10 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens;
+    use HasFactory;
+    use HasUuid;
+    use Notifiable;
 
     /**
      * The attributes that are mass assignable.
